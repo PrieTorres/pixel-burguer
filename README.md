6abec353289da15236bd17a711ee125462d3299b
Priscila <priscilatorres2023@gmail.com>
2024-07-08 17:27:54 -0300

    up

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Nothing here yet.........
+
+https://www.youtube.com/watch?v=wm5gMKuwSYk
+
+to do: 
+*implement multiple language
---
+++ src/app/orders/[orderId]/page.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+import { Container } from './styles';
+
+export const Order = (): ReactElement => {
+
+  return(
+    <Container>
+      {orderId}
+    </Container>
+  )
+};

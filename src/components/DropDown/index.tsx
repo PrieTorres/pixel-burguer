5b49f2463c2ad0d60292fd8b4925b0f5ec568c7f
Priscila <priscilatorres2023@gmail.com>
2024-08-21 14:45:49 -0300

    Update index.tsx

--- src/components/DropDown/index.tsx
+++ src/components/DropDown/index.tsx
@@ -1,5 +1,5 @@
-/*"use client";
-import { MouseEventHandler, ReactElement, ReactNode, useState } from 'react';
+"use client";
+import { MouseEventHandler, MutableRefObject, ReactElement, ReactNode, useEffect, useRef, useState } from 'react';
 import { Container } from './styles';
 import { IconSpan } from '../IconSpan';
 import ClientPortal from '../Portal';
@@ -10,6 +10,23 @@ export interface DropItemProps {
   children: ReactNode;
 }
 
+export interface DropDownListProps {
+  dropDownId: string;
+  toggleId: string;
+  items: Array<DropItemProps>;
+  height?: number;
+  isOpen: boolean;
+  toggleFunction: CallableFunction;
+  offset?: {
+    top?: number;
+    bottom?: number;
+    left?: number;
+    right?: number;
+    height?: number;
+    width?:number;
+  };
+}
+
 export interface DropDownProps {
   children: ReactNode;
   dropDownId: string;
@@ -18,50 +35,75 @@ export interface DropDownProps {
   height?: number;
 }
 
+const DropDownList = ({ isOpen, offset, dropDownId, toggleId, items, toggleFunction }: DropDownListProps) => {
+  const transClass = isOpen ? "flex" : "hidden";
+  const style = offset ? {
+    ...offset,
+    top: (offset.top || 0) + (offset.height || 0),
+    left: offset.left
+  } : {};
+
+
+  return <ClientPortal>
+    <div style={style} id={toggleId} className={`flex flex-col rounded-md ${transClass}`}>
+      <ul aria-labelledby={dropDownId}>
+        {items.map((dropItem, i) => (
+          <li key={`drop-item-${i}_${Math.floor(Math.random() * 99999)}`}>
+            <div onClick={(e) => { dropItem.onClick(e); toggleFunction(); }}>
+              {dropItem.children}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </ClientPortal>;
+};
+
 export const DropDown = ({ children, dropDownId, toggleId, items, height }: DropDownProps): ReactElement => {
-  const returnComponent = () => {
-    const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [domReady, setDomReady] = useState<boolean>(false);
+  const container: MutableRefObject<null | HTMLDivElement> = useRef(null);
+  const boundaries: MutableRefObject<{} | DOMRect> = useRef({});
 
   const toggle = () => {
     setIsOpen(old => !old);
   };
 
-  const transClass = isOpen ? "flex" : "hidden";
+  useEffect(() => {
+    setDomReady(true);
+
+  }, []);
+  
+  useEffect(() => {
+    if (typeof container?.current?.getBoundingClientRect == "function") {
+      console.log(container?.current?.getBoundingClientRect());
+      boundaries.current = container?.current?.getBoundingClientRect();
+    }
+  }, []);
+  //const transClass = isOpen ? "flex" : "hidden";
 
   return (
-    
-      <Container height={height}>
-        <button
-          id={dropDownId}
-          data-dropdown-toggle={toggleId}
-          data-dropdown-delay="500"
-          className="
+    <Container height={height} ref={container}>
+      <button
+        id={dropDownId}
+        data-dropdown-toggle={toggleId}
+        data-dropdown-delay="500"
+        className="
           text-center 
           inline-flex 
           items-center"
-          onClick={toggle}
-        >
-          {children} <IconSpan icon="arrow_drop_down" />
-        </button>
-        <div id={toggleId} className={`flex flex-col rounded-md ${transClass}`}>
-          <ul aria-labelledby={dropDownId}>
-            {items.map((dropItem, i) => (
-              <li key={`drop-item-${i}_${Math.floor(Math.random() * 99999)}`}>
-                <div onClick={(e) => { dropItem.onClick(e); toggle(); }}>
-                  {dropItem.children}
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </Container>
-    
+        onClick={toggle}
+      >
+        {children} <IconSpan icon="arrow_drop_down" />
+      </button>
+      {domReady && <DropDownList
+        toggleFunction={toggle}
+        items={items}
+        isOpen={isOpen}
+        dropDownId={dropDownId}
+        toggleId={toggleId}
+        offset={boundaries.current}
+      />}
+    </Container>
   );
-  }
-
-  
-    return createPortal(returnComponent(),
-                                  document.querySelector('#default-portal') as Element
-                                )
-  
-};*/
+};

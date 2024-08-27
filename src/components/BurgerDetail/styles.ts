b554219f22e0fd2023e0d8ed513492501fe8d88f
Priscila <priscilatorres2023@gmail.com>
2024-08-27 19:57:32 -0300

    Update styles.ts

--- src/components/BurgerDetail/styles.ts
+++ src/components/BurgerDetail/styles.ts
@@ -1,9 +1,18 @@
 'use client'
+import { responsiveFontSize } from '@/Styles/helperStyles';
 import styled, { DefaultTheme, css } from 'styled-components';
 
 export const Container = styled.article`
   ${({ theme }: { theme: DefaultTheme }) => css`
-     
+     display: flex;
+     flex-direction: column;
+     align-items: center;
+     justify-content: space-between;
+     gap: 8px;
+
+     & > h2 {
+      ${responsiveFontSize(theme, "large")}
+     }
      
   `}
 `;
